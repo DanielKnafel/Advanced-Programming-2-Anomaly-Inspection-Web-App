abb39994821227f2e15d31f78cf42a6d8da1adca
Daniel <danielknafel@gmail.com>
2021-05-04 03:14:53 -0700

    Added c++ server and a web server

---
+++ AnomalyDetectionServer/MainTrain.cpp
@@ -0,0 +1,56 @@
+/*
+ * run2.cpp
+ *
+ *  Created on: 8 ����� 2019
+ *      Author: Eli
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <thread>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include "Server.h"
+
+using namespace std;
+
+void writeStr(string input,int serverFD){
+	write(serverFD,input.c_str(),input.length());
+	write(serverFD,"\n",1);
+}
+
+string readStr(int serverFD){
+	string serverInput="";
+	char c=0;
+	read(serverFD,&c,sizeof(char));
+	while(c!='\n'){
+		serverInput+=c;
+		read(serverFD,&c,sizeof(char));
+	}
+	return serverInput;
+}
+
+int main(){
+	int port=5555;
+
+	try{
+		AnomalyDetectionHandler adh;
+		Server server(port);
+		server.start(adh); // runs on its own thread
+		server.stop();
+	}catch(const char* s){
+		cout<<s<<endl;
+	}
+
+
+	cout<<"done"<<endl;
+	return 0;
+}
