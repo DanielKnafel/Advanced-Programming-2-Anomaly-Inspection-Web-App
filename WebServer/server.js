67866b2cdfd54b5f5d9af4d2ee239ae5b71bbd78
chantzish <chantzish@gmail.com>
2021-05-05 17:53:50 +0300

    body-parser deprecated fix

--- WebServer/server.js
+++ WebServer/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
 
 var id=0;
 var models={};
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/test", (req, res) => {
     var net = require('net');
     var client = new net.Socket();
