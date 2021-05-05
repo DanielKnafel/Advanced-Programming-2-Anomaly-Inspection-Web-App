db36cfa36fe5b2a727299a43be1f2b3cb78d5852
chantzish <chantzish@gmail.com>
2021-05-05 15:27:59 +0300

    removed build and .vscode folders

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/linux/Desktop/Advanced-Programming-2-Anomaly-Inspection-Web-App/AnomalyDetectionServer
-# Build directory: /home/linux/Desktop/Advanced-Programming-2-Anomaly-Inspection-Web-App/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
