68313abcc397567b831290f66aba976057a76631
chantzish <chantzish@gmail.com>
2021-05-30 19:35:28 +0300

    fixed storyTwo.py

--- WebServer/storyTwo/storyTwo.py
+++ WebServer/storyTwo/storyTwo.py
@@ -1,16 +1,13 @@
 import requests
 
 url = 'http://localhost:9876/detect'
-myobj = {'somekey': 'Simple'}
 
-x = requests.post(url, data = myobj)
-test_files = {
-    "algorithm": 'Simple',
+form_data = {
+    "algorithm": (None, 'Simple'),
     "learnFile": open("train.csv", "rb"),
     "detectFile": open("test.csv", "rb")
 }
 
-test_response = requests.post(url=url, files = test_files)
-
+test_response = requests.post(url=url, files = form_data)
 
 print(test_response.text)
